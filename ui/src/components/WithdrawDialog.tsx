aa97a08b2dbbc1e7e6c4df9920cd7c54ebe011c0
boscoeut <boscoeut@gmail.com>
2023-12-31 08:13:28 -0700

    Add withdrawal details and balance calculation to WithdrawDialog component

--- ui/src/components/WithdrawDialog.tsx
+++ ui/src/components/WithdrawDialog.tsx
@@ -14,10 +14,12 @@ import ModalDialog from '@mui/joy/ModalDialog';
 import Option from '@mui/joy/Option';
 import Select from '@mui/joy/Select';
 import Stack from '@mui/joy/Stack';
+import KLabel from './KLabel';
+import Table from '@mui/joy/Table';
 import * as React from 'react';
 import { AMOUNT_DECIMALS, EXCHANGE_POSITIONS } from "utils/dist/constants";
 import { useKrunchStore } from "../hooks/useKrunchStore";
-import { formatCurrency } from '../utils';
+import { formatCurrency, formatNumber } from '../utils';
 
 export interface WithdrawDialogProps {
   open: boolean;
@@ -26,11 +28,17 @@ export interface WithdrawDialogProps {
 
 export default function WithdrawDialog({ open, setOpen }: WithdrawDialogProps) {
   const [market, setMarket] = React.useState('USDC/USD');
-  const [amount, setAmount] = React.useState('1000');
+  const [amount, setAmount] = React.useState('0');
   const [submitting, setSubmitting] = React.useState(false);
+  const userBalances = useKrunchStore(state => state.userBalances)
   const withdraw = useKrunchStore(state => state.withdraw)
   const userAccountValue = useKrunchStore(state => state.userAccountValue)
+  const withdrawValue=Number(amount) 
+  const selectedMarket = userBalances.find((position) => position.market === market)
+  const balanceAfterWithdraw = userAccountValue/AMOUNT_DECIMALS - withdrawValue
 
+  const tokenAmount = Number(amount) / (selectedMarket?.price || 0)
+  const tokenPhrase = `Receive ${formatNumber(tokenAmount,5)} ${selectedMarket?.market.replace("/USD","")} Tokens`
   const handleSubmit = async () => {
     try {
       setSubmitting(true)
@@ -43,23 +51,28 @@ export default function WithdrawDialog({ open, setOpen }: WithdrawDialogProps) {
     }
   };
 
-  const properties = [
-    { label: 'Amount', value: amount, onChange: setAmount, type: 'number' },
-    { label: "Token to Receive", value: market, onChange: setMarket, type: 'markets' },
-  ]
-
   let submitMessage = 'Withdraw'
   let errorMessage = ''
   let canSubmit = !submitting
-
   const selectedBalance = userAccountValue / AMOUNT_DECIMALS
-  if (selectedBalance < Number(amount)) {
+  if (Number(selectedBalance.toFixed(2)) < Number(amount)) {
     canSubmit = false
     submitMessage = 'Insufficient Balance'
     errorMessage = submitMessage
   }
+
+  if ( Number(amount)<=0) {
+    canSubmit = false
+    submitMessage = 'Amount must be greater than 0'
+    errorMessage = submitMessage
+  }
+
   if (submitting) {
     submitMessage = 'Withdrawing...'
+  }
+
+  const setMax = () => {
+    setAmount(Number(userAccountValue / AMOUNT_DECIMALS).toFixed(2))
   }
 
   return (
@@ -77,7 +90,7 @@ export default function WithdrawDialog({ open, setOpen }: WithdrawDialogProps) {
           >
             <Stack spacing={2}>
               <FormControl error={!canSubmit}>
-                <FormLabel>Amount <Chip color="success">Max {formatCurrency(userAccountValue / AMOUNT_DECIMALS)} </Chip></FormLabel>
+                <FormLabel>Amount <Chip onClick={setMax} color="success">Max {formatCurrency(userAccountValue / AMOUNT_DECIMALS)} </Chip></FormLabel>
                 <Input autoFocus required value={amount} onChange={(e: any) => setAmount(e.target.value)} />
                 {!canSubmit && <FormHelperText>
                   <InfoOutlined />
@@ -90,11 +103,30 @@ export default function WithdrawDialog({ open, setOpen }: WithdrawDialogProps) {
                   setMarket(newValue)
                 }}>
                   {EXCHANGE_POSITIONS.map((position) => {
-                    return <Option key={position.market} value={position.market} >{position.market}</Option>
+                    return <Option key={position.market} value={position.market} >{position.market.replace("USD/","")}</Option>
                   })}
                 </Select>
               </FormControl>
               <Button disabled={!canSubmit} type="submit">{submitMessage}</Button>
+
+              <FormControl>
+                <Table>
+                <tbody>
+                  <tr>
+                    <td style={{width:175}}>Current Balance</td>
+                    <td>{formatCurrency(userAccountValue/AMOUNT_DECIMALS)}</td>
+                  </tr>
+                  <tr>
+                    <td>Withdraw Amount</td>
+                    <td><KLabel numValue={Number(amount)*-1}>{formatCurrency(withdrawValue)}</KLabel> <span style={{paddingLeft:10, fontSize:'0.9em'}}>({tokenPhrase}) (Current Price = {formatNumber(selectedMarket?.price || 0,4)})</span></td>
+                  </tr>
+                  <tr>
+                    <td>Balance After Withdraw</td>
+                    <td><KLabel fontWeight='bold' numValue={balanceAfterWithdraw}>{formatCurrency(balanceAfterWithdraw)}</KLabel></td> 
+                  </tr>
+                </tbody>
+                </Table>                  
+              </FormControl>
             </Stack>
           </form>
         </ModalDialog>
